8b19d4e42667aeb8a39551621c7399a3e615403e
averdin2 <alexverdin01@gmail.com>
2019-07-11 16:27:18 -0700

    Initial Commit. Babel, react, and webpack are configured. Hello World is displayed.

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const HWP = require('html-webpack-plugin');
+module.exports = {
+  module:{
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader'
+          }
+        ]
+      }
+    ]
+  },
+  plugins:[
+    new HWP({
+      template: "./src/index.html",
+      filename: "./index.html"
+    })
+  ]
+}
