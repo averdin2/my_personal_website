df19e7b70348d09f2302764eca6cee7324873ff2
averdin2 <alexverdin01@gmail.com>
2019-08-07 16:22:19 -0700

    Styling for all components is complete. Only thing left is to fill text content of components.

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -8,24 +8,24 @@ export default function Projects() {
       <div className='inner-projects-container'>
         <h1>Projects</h1>
           <div className='project crookbook'>
-            <h3>CrookBook</h3>
+            <h2>CrookBook</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-            <a href='#' className='button'>See Code</a>
+            <a href='https://github.com/evanhessler/CrookBook' className='button'>See Code</a>
           </div>
           <div className='project dodgers'>
-            <h3>Dodgers Scoreboard</h3>
+            <h2>Dodgers Scores</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-            <a href='#' className='button'>See Code</a>
+            <a href='https://github.com/averdin2/Dodgers_Scores' className='button'>See Code</a>
           </div>
           <div className='project mlb'>
-            <h3>MLB Player Stats</h3>
+            <h2>MLB Player Stats</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-            <a href='#' className='button'>See Code</a>
+            <a href='https://github.com/averdin2/MLB_Stats' className='button'>See Code</a>
           </div>
           <div className='project paneljumper'>
-            <h3>Panel Jumper</h3>
+            <h2>Panel Jumper</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-            <a href='#' className='button'>See Code</a>
+            <a href='https://github.com/averdin2/Panel-Jumper' className='button'>See Code</a>
           </div>
       </div>
     </div>
