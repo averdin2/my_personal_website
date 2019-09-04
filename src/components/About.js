83b1628a4a431b561877faf2b5b44a0c79759517
averdin2 <alexverdin01@gmail.com>
2019-09-04 15:43:11 -0700

    Changed some about text.

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,7 @@ export default function About() {
       <div className='about-text'>
         <h1>About Me</h1>
         <p>
-        Since a young age, I have been fascinated by the capabilities of computers and the power of programming. After diving into my first hello world app, I have thoroughly come to enjoy the challenges and creativity that comes with software development. I am passionate about my work and I strive to see the projects I am a part of succeed. I prioritize sound judgement, integrity, and quality work in everything that I do. I am constantly seeking to improve my skills and learn new technologies in this ever-changing landscape. When I am not coding, I enjoy watching Dodgers games or mixing myself a drink and listening to the latest “Front End Happy Hour” podcast. For any inquiries feel free to shoot me an email or message me on LinkedIn.       
+        Since a young age, I have been fascinated by the capabilities of computers and the power of programming. After diving into my first hello world app, I have thoroughly come to enjoy the challenges and creativity that comes with software development. I am passionate about my work and I strive to see the projects I am a part of succeed. I prioritize sound judgement, integrity, and quality work in everything that I do. I am constantly seeking to improve my skills and learn new technologies in this ever-changing landscape. When I am not coding, I enjoy watching Dodgers games or listening to a new episode of a coding podcast. For any inquiries feel free to shoot me an email or message me on LinkedIn.       
         </p>
       </div>
     </div>
