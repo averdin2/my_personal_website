da10d13b391d3b24d465cd651c619e0f66cec20c
averdin2 <alexverdin01@gmail.com>
2020-02-24 14:39:59 -0800

    got rid of email in contact page for safety. Going to add a contact form in the future.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,23 +3,21 @@ import '../styles/contact.scss';
 
 export default function Contact() {
   return (
-    <div id='contact' className='contact-container'>
-      <div className='line'></div>
-      <div className='inner-contact-container'>
+    <div id="contact" className="contact-container">
+      <div className="line"></div>
+      <div className="inner-contact-container">
         <h1>Contact</h1>
-        <div className='contact'>
-          <h2>Email</h2>
-          <p>alexverdin01@gmail.com</p>
-        </div>
-        <div className='contact'>
+        <div className="contact">
           <h2>LinkedIn</h2>
-          <a href='https://www.linkedin.com/in/alex-v-verdin/'>Check my LinkedIn here</a>
+          <a href="https://www.linkedin.com/in/alex-v-verdin/">
+            Check my LinkedIn here
+          </a>
         </div>
-        <div className='contact'>
+        <div className="contact">
           <h2>GitHub</h2>
-          <a href='https://github.com/averdin2'>Check my GitHub here</a>
+          <a href="https://github.com/averdin2">Check my GitHub here</a>
         </div>
       </div>
     </div>
-  )
+  );
 }
