0e8bd1e767635b38e71701fc48d9a060cef7ec87
averdin2 <alexverdin01@gmail.com>
2020-05-12 17:01:35 -0700

    Added link for covid project

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -16,6 +16,7 @@ export default function Projects() {
         <div className="project covid">
           <h2>Covid-19 Charts</h2>
           <p>A website that provides data charts for Covid-19 cases.</p>
+          <a href='https://averdin2.github.io/Covid-19-data-charts/' className='button'>See Site</a>
           <a href='https://github.com/averdin2/Covid-19-data-charts' className='button'>See Code</a>
         </div>
         <div className='project dodgers'>
