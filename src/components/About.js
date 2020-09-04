3a750bf181cdec636600e6d61d70bec04937b4aa
averdin2 <alexverdin01@gmail.com>
2020-09-04 12:39:55 -0700

    Updating dependencies, updating about me section, and adding Pomodoro Timer to projects section.

--- src/components/About.js
+++ src/components/About.js
@@ -3,13 +3,22 @@ import '../styles/about.scss';
 
 export default function About() {
   return (
-    <div id='about' className='about'>
-      <div className='about-text'>
+    <div id="about" className="about">
+      <div className="about-text">
         <h1>About Me</h1>
         <p>
-        Since a young age, I have been fascinated by the capabilities of computers and the power of programming. After diving into my first hello world app, I have thoroughly come to enjoy the challenges and creativity that comes with software development. I am passionate about my work and I strive to see the projects I am a part of succeed. I prioritize sound judgement, integrity, and quality work in everything that I do. I am constantly seeking to improve my skills and learn new technologies in this ever-changing landscape. When I am not coding, I enjoy watching Dodgers games or listening to a new episode of a coding podcast. For any inquiries feel free to message me on LinkedIn.       
+          Since a young age, I have been fascinated by the capabilities of
+          computers and the power of programming. After diving into my first
+          hello world app, I have thoroughly come to enjoy the challenges and
+          creativity that comes with software development. I am a Front-End web
+          developer with a passion for web design and user experience. I am
+          primarily focused on making responsive React web applications, but I
+          like to dabble in various other web technologies when I get the
+          chance. I am always seeking ways to improve my skills and learn
+          something new. If you would like to contact me, feel free to message
+          me on LinkedIn.
         </p>
       </div>
     </div>
-  )
+  );
 }
