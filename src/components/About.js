5766d9bdcf11b8bea2bdacfe1c79cdeaed7bec65
averdin2 <alexverdin01@gmail.com>
2021-09-21 12:01:03 -0700

    Made some changes to projects on site

--- src/components/About.js
+++ src/components/About.js
@@ -5,18 +5,13 @@ export default function About() {
   return (
     <div id="about" className="about">
       <div className="about-text">
-        <h1>About Me</h1>
+        <h1 className="about-header">About Me</h1>
         <p>
-          Since a young age, I have been fascinated by the capabilities of
-          computers and the power of programming. After diving into my first
-          hello world app, I have thoroughly come to enjoy the challenges and
-          creativity that comes with software development. I am a Front-End web
-          developer with a passion for web design and user experience. I am
-          primarily focused on making responsive React web applications, but I
-          like to dabble in various other web technologies when I get the
-          chance. I am always seeking ways to improve my skills and learn
-          something new. If you would like to contact me, feel free to message
-          me on LinkedIn.
+          Since a young age, I have been fascinated by the capabilities of computers and the power of programming. After diving into my first hello world app, I
+          have thoroughly come to enjoy the challenges and creativity that comes with software development. I am a Front-End web developer with a passion for
+          web design and user experience. I am primarily focused on making responsive React web applications, but I like to dabble in various other web
+          technologies when I get the chance. I am always seeking ways to improve my skills and learn something new. If you would like to contact me, feel free
+          to message me on LinkedIn.
         </p>
       </div>
     </div>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,57 +7,21 @@ export default function Projects() {
       <div className="line"></div>
       <h1>Projects</h1>
       <div className="inner-projects-container">
-        <div className="project crookbook">
-          <h2>CrookBook</h2>
-          <p>
-            A web application database to help the LAPD sort thier old murder
-            case files.{' '}
-          </p>
-          <a href="https://github.com/evanhessler/CrookBook" className="button">
-            See Code
-          </a>
-        </div>
-        <div className="project covid">
-          <h2>Covid-19 Charts</h2>
-          <p>A website that provides data charts for Covid-19 cases.</p>
+        <div className="project pomodoro">
+          <h2>Pomodoro Timer</h2>
+          <p>A React web app that acts as a working Pomodoro Timer. </p>
           <div className="buttons">
-            <a
-              href="https://averdin2.github.io/Covid-19-data-charts/"
-              className="button"
-            >
+            <a href=" https://averdin2.github.io/React_Pomodoro/" className="button">
               See Site
             </a>
-            <a
-              href="https://github.com/averdin2/Covid-19-data-charts"
-              className="button"
-            >
-              See Code
-            </a>
-          </div>
-        </div>
-        <div className="project dodgers">
-          <h2>Dodgers Scores</h2>
-          <p>A website that provides gameday scores for live Dodgers Games. </p>
-          <div className="buttons">
-            <a
-              href="https://averdin2.github.io/Dodgers_Scores/"
-              className="button"
-            >
-              See Site
-            </a>
-            <a
-              href="https://github.com/averdin2/Dodgers_Scores"
-              className="button"
-            >
+            <a href="https://github.com/averdin2/React_Pomodoro" className="button">
               See Code
             </a>
           </div>
         </div>
         <div className="project mlb">
           <h2>MLB Player Stats</h2>
-          <p>
-            A website for stats of all current MLB players sorted by their team.{' '}
-          </p>
+          <p>A website for stats of all current MLB players sorted by their team. </p>
           <div className="buttons">
             <a href="https://averdin2.github.io/MLB_Stats/" className="button">
               See Site
@@ -67,30 +31,31 @@ export default function Projects() {
             </a>
           </div>
         </div>
+        <div className="project covid">
+          <h2>Covid-19 Charts</h2>
+          <p>A website that provides data charts for Covid-19 cases.</p>
+          <div className="buttons">
+            <a href="https://github.com/averdin2/Covid-19-data-charts" className="button">
+              See Code
+            </a>
+          </div>
+        </div>
+        <div className="project dodgers">
+          <h2>Dodgers Scores</h2>
+          <p>A website that provides gameday scores for Los Angeles Dodger's Games. </p>
+          <div className="buttons">
+            <a href="https://github.com/averdin2/Dodgers_Scores" className="button">
+              See Code
+            </a>
+          </div>
+        </div>
+
         <div className="project paneljumper">
           <h2>Panel Jumper</h2>
-          <p>
-            A 2D infinite jumper game seeking to add a unique take to the genre.{' '}
-          </p>
+          <p>A 2D infinite jumper game seeking to add a unique take to the genre. </p>
           <a href="https://github.com/averdin2/Panel-Jumper" className="button">
             See Code
           </a>
-        </div>
-        <div className="project pomodoro">
-          <h2>Pomodoro Timer</h2>
-          <p>A React web app that acts as a working Pomodoro Timer. </p>
-          <div className="buttons">
-          <a href=" https://averdin2.github.io/React_Pomodoro/" className="button">
-              See Site
-            </a>
-          <a
-            href="https://github.com/averdin2/React_Pomodoro"
-            className="button"
-          >
-            See Code
-          </a>
-          </div>
-          
         </div>
       </div>
     </div>
